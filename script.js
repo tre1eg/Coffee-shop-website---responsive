b9a4c622a9460b44fd186b63986d5a4a77af32c7
Tre1eg <abhonkarharsh12@gmail.com>
2023-09-07 09:10:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+document.querySelectorAll('.image-slider img').forEach(images =>{
+    images.onclick = () => {
+        var src = images.getAttribute('src');
+        document.querySelector('.main-home-image').src = src;
+    };
+});
+
+var swiper = new Swiper(".review-slider",{
+    spaceBetween: 20,
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true
+    },
+    loop: true,
+    grabCursor:true,
+    autoplay : {
+        delay : 7500,
+        disableInteraction : false,
+    },
+    breakpoints : {
+        0:{
+            slidesPerView: 1 ,
+        },
+        768 : {
+            slidesPerView : 2,
+        }, 
+    },
+});
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+Window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
